0eeb4f4c6fe44644f4f8471609a88e770d162b75
Bettkurt <betullkk342@gmail.com>
2023-05-12 23:12:25 +0300

    updated

---
+++ ndpödevson/Class1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ndpödevson
+{
+    public class Nesneler<nesne>
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public int Cap;
+        public int Yukseklik;
+        public int Genislik;
+        public int derinlik;
+    }
+
+    class Nokta<nesne> : Nesneler<nesne>
+    {
+        public Nokta()
+        { 
+        Random random = new Random(Guid.NewGuid().GetHashCode());
+        X = random.Next(1,683);
+        Y= random.Next(1, 420);
+        Z = random.Next(1,683);
+       }
+    public void NoktaCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.DrawEllipse(pen, X, Y, 1, 1);
+
+        }
+    }
+
+    class Dörtgen<nesne> : Nesneler<nesne>
+    {
+        public Dörtgen()
+        {
+        Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(50, 200);
+            Y = random.Next(50, 200);
+            Yukseklik = random.Next(50, 200);
+            Genislik = random.Next(50, 200);
+        }
+        public void DörtgenCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+        }
+
+    }
+    class Dörtgen2<nesne> : Nesneler<nesne>
+    {
+        public Dörtgen2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Yukseklik = random.Next(50, 200);
+            Genislik = random.Next(50, 200);
+        }
+        public void Dörtgen2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+        }
+
+    }
+    class Dikdörtgen<nesne> : Nesneler<nesne>
+    {
+        public Dikdörtgen()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Yukseklik = random.Next(20, 200);
+            Genislik = random.Next(20, 200);
+        }
+        public void DikdörtgenCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+        }
+
+    }  
+    class Dikdörtgen2<nesne> : Nesneler<nesne>
+    {
+        public Dikdörtgen2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Yukseklik = random.Next(1, 200);
+            Genislik = random.Next(1, 200);
+        }
+        public void Dikdörtgen2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+        }
+
+    }
+    class Cember<nesne> : Nesneler<nesne>
+    {
+        public Cember()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(50, 300);
+            Y = random.Next(50, 300);
+            Cap = random.Next(1, 300);
+        }
+        public void CemberCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.DrawEllipse(pen, X, Y, Cap,Cap);
+
+        }
+
+    }
+    class Cember2<nesne> : Nesneler<nesne>
+    {
+        public Cember2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 300);
+            Y = random.Next(1, 300);
+            Cap = random.Next(1, 300);
+        }
+        public void Cember2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+
+            graphics.DrawEllipse(pen, X, Y, Cap, Cap);
+
+        }
+
+    }
+
+    class Prizma<nesne> : Nesneler<nesne>
+    {
+        public Prizma()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 300);
+            Y = random.Next(1,200);
+            Z = random.Next(1, 200);
+            Yukseklik = random.Next(1, 200);
+            Genislik = random.Next(1, 200);
+            derinlik = random.Next(1, 200);
+        }
+        public void PrizmaCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            //alt yuzey
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+            //üst yuzey
+            graphics.DrawRectangle(pen, X + derinlik, Y + derinlik, Genislik, Yukseklik);
+
+            //yan yüzeyler
+            graphics.DrawLine(pen, X, Y, X + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y, X + Genislik + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X, Y + Yukseklik, X + derinlik, Y + Yukseklik + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y + Yukseklik, X + Genislik + derinlik, Y + Yukseklik + derinlik);
+
+        }
+
+    }
+    class Prizma2<nesne> : Nesneler<nesne>
+    {
+        public Prizma2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 100);
+            Z = random.Next(1, 200);
+            Yukseklik = random.Next(1,200);
+            Genislik = random.Next(1, 200);
+            derinlik = random.Next(1, 200);
+        }
+        public void Prizma2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+
+            //alt yuzey
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+            //üst yuzey
+            graphics.DrawRectangle(pen, X + derinlik, Y + derinlik, Genislik, Yukseklik);
+
+            //yan yüzeyler
+            graphics.DrawLine(pen, X, Y, X + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y, X + Genislik + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X, Y + Yukseklik, X + derinlik, Y + Yukseklik + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y + Yukseklik, X + Genislik + derinlik, Y + Yukseklik + derinlik);
+
+        }
+
+    }
+    class Küre<nesne> : Nesneler<nesne>
+    {
+        public Küre()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Z = random.Next(1, 200);
+            Cap = random.Next(1,200);
+        }
+        public void KüreCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.FillEllipse(Brushes.DarkGray,X ,Y  , Cap , Cap );
+
+        }
+
+    }
+    class Küre2<nesne> : Nesneler<nesne>
+    {
+        public Küre2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Z = random.Next(1, 200);
+            Cap = random.Next(1, 200);
+        }
+        public void Küre2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.FillEllipse(Brushes.DarkGray, X, Y, Cap, Cap);
+
+        }
+
+    }
+    class Silindir<nesne> : Nesneler<nesne>
+    {
+        public Silindir()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Z = random.Next(1, 200);
+            Cap = random.Next(1, 200);
+            Yukseklik = random.Next(1, 300);
+        }
+        public void SilindirCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            graphics.DrawEllipse(pen, X , Y , Cap , Cap );
+
+            graphics.DrawEllipse(pen, X , Y + Yukseklik , Cap , Cap );
+
+            graphics.DrawLine(pen, X, Y + Cap / 2 , X , Y + Yukseklik + Cap/2);
+            graphics.DrawLine(pen, X + Cap, Y + Cap/2, X + Cap, Y + Yukseklik + Cap/2);
+
+        }
+
+    }
+    class Silindir2<nesne> : Nesneler<nesne>
+    {
+        public Silindir2()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(1, 200);
+            Y = random.Next(1, 200);
+            Z = random.Next(1, 200);
+            Cap = random.Next(1,200);
+            Yukseklik = random.Next(1, 170);
+        }
+        public void Silindir2Ciz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+
+            graphics.DrawEllipse(pen, X, Y, Cap, Cap);
+
+            graphics.DrawEllipse(pen, X, Y + Yukseklik, Cap, Cap);
+
+            graphics.DrawLine(pen, X, Y + Cap / 2, X, Y + Yukseklik + Cap / 2);
+            graphics.DrawLine(pen, X + Cap, Y + Cap / 2, X + Cap, Y + Yukseklik + Cap / 2);
+
+        }
+
+    }
+    class Yuzey<nesne> : Nesneler<nesne>
+    {
+        public Yuzey()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            X = random.Next(60, 200);
+            Y = random.Next(1, 200);
+            Z = random.Next(1, 100);
+            Yukseklik = random.Next(1, 200);
+            Genislik = 1;
+            derinlik = random.Next(1, 200);
+        }
+        public void YuzeyCiz(Panel panel)
+        {
+            Graphics graphics = panel.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            //alt yuzey
+            graphics.DrawRectangle(pen, X, Y, Genislik, Yukseklik);
+
+            //üst yuzey
+            graphics.DrawRectangle(pen, X + derinlik, Y + derinlik, Genislik, Yukseklik);
+
+            //yan yüzeyler
+            graphics.DrawLine(pen, X, Y, X + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y, X + Genislik + derinlik, Y + derinlik);
+            graphics.DrawLine(pen, X, Y + Yukseklik, X + derinlik, Y + Yukseklik + derinlik);
+            graphics.DrawLine(pen, X + Genislik, Y + Yukseklik , X  + Genislik + derinlik, Y  + Yukseklik + derinlik);
+
+        }
+
+    }
+
+
+
+
+}
---
+++ ndpödevson/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ndpödevson 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Nokta<int> nokta = new Nokta<int>();
+            Dörtgen<int> dortgen = new Dörtgen<int>();
+
+            dortgen.DörtgenCiz(panel1 );
+            nokta.NoktaCiz(panel1);
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Nokta<int> nokta = new Nokta<int>();
+            Cember<int> cember = new Cember<int>();
+
+            
+            nokta.NoktaCiz(panel1);
+            cember.CemberCiz(panel1);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Dörtgen<int> dortgen = new Dörtgen<int>();
+            Dörtgen2<int> dortgen2 = new Dörtgen2<int>();
+
+            dortgen.DörtgenCiz(panel1);
+            dortgen2.Dörtgen2Ciz(panel1);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Dörtgen<int> dortgen = new Dörtgen<int>();
+            Cember<int> cember = new Cember<int>();
+
+            dortgen.DörtgenCiz(panel1);
+            cember.CemberCiz(panel1);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Cember<int> cember = new Cember<int>();
+            Cember2<int> cember2 = new Cember2<int>();
+
+            cember.CemberCiz(panel1);
+            cember2.Cember2Ciz(panel1);
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh(); 
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Nokta<int> nokta = new Nokta<int>();
+            Küre<int> kure = new Küre<int>();
+
+            nokta.NoktaCiz(panel1);
+            kure.KüreCiz(panel1);
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Nokta<int> nokta = new Nokta<int>();
+            Prizma<int> prizma = new Prizma<int>();
+
+            nokta.NoktaCiz(panel1);
+            prizma.PrizmaCiz(panel1);
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Nokta<int> nokta = new Nokta<int>();
+            Silindir<int> silindir = new Silindir<int>();
+
+            nokta.NoktaCiz(panel1);
+            silindir.SilindirCiz(panel1);
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Silindir<int> silindir = new Silindir<int>();
+            Silindir2<int> silindir2 = new Silindir2<int>();
+
+            silindir.SilindirCiz(panel1);
+            silindir2.Silindir2Ciz(panel1);
+
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Küre<int> kure = new Küre<int>();
+            Küre2<int> kure2 = new Küre2<int>();
+
+            kure.KüreCiz(panel1);
+            kure2.Küre2Ciz(panel1);
+        }
+
+        private void radioButton11_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Küre<int> kure = new Küre<int>();
+            Silindir<int> silindir = new Silindir<int>();
+
+            kure.KüreCiz(panel1);
+            silindir.SilindirCiz(panel1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton12_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Yuzey<int> yuzey = new Yuzey<int>();
+            Küre<int> kure = new Küre<int>();
+
+            yuzey.YuzeyCiz(panel1);
+            kure.KüreCiz(panel1);
+        }
+
+        private void radioButton13_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Yuzey<int> yuzey = new Yuzey<int>();
+            Prizma<int> prizma = new Prizma<int>();
+
+            yuzey.YuzeyCiz(panel1);
+            prizma.PrizmaCiz(panel1);
+        }
+
+        private void radioButton14_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Yuzey<int> yuzey = new Yuzey<int>();
+            Silindir<int> silindir = new Silindir<int>();
+
+            yuzey.YuzeyCiz(panel1);
+            silindir.SilindirCiz(panel1);
+
+        }
+
+        private void radioButton15_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh(); 
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Küre<int> kure = new Küre<int>();
+            Prizma<int> prizma = new Prizma<int>();
+
+            kure.KüreCiz(panel1);
+            prizma.PrizmaCiz(panel1);
+
+        }
+
+        private void radioButton16_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            panel1.Refresh();
+
+            List<Nesneler<int>> nesneler = new List<Nesneler<int>>();
+            Prizma<int> prizma = new Prizma<int>();
+            Prizma2<int> prizma2 = new Prizma2<int>();
+
+            prizma.PrizmaCiz(panel1);
+            prizma2.Prizma2Ciz(panel1);
+        }
+    }
+}
